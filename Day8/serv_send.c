aaaefcd70a8d18cfa2abccd5ce67830d536efe3a
junnamuzaurusu <juntaro.sonoda@gmail.com>
2025-05-29 14:55:35 +0900

    本課題8.1

---
+++ Day8/serv_send.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define N 1024
+
+int main(int argc, char *argv[]){
+    if(argc != 2){
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    unsigned char data[N];
+    int ss = socket(PF_INET, SOCK_STREAM, 0);
+    if(ss == -1){
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(argv[1]));
+    addr.sin_addr.s_addr = INADDR_ANY;
+    bind(ss, (struct sockaddr *)&addr, sizeof(addr));
+
+    listen(ss, 10);
+
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+    int s = accept(ss, (struct sockaddr *)&client_addr, &len);
+    if(s == -1){
+        perror("accept");
+        exit(1);
+    }
+
+    while(1){
+        int n = read(0, data, N);
+        if(n == -1){
+            perror("read");
+            exit(1);
+        }
+        if(send(s, data, n, 0) == -1){
+            perror("send");
+        }
+        if(n == 0){
+            break;
+        }
+    }
+    close(s);
+    close(ss);
+}
